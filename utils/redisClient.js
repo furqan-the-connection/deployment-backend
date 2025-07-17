3396a940b9acab99da215ba10c431ccb8c2e0bd2
Furqan <furqan4fev2004khan@gmail.com>
2025-07-17 15:46:56 +0500

    pushing the code

---
+++ utils/redisClient.js
@@ -0,0 +1,12 @@
+import { createClient } from "redis";
+
+const redisClient = createClient({
+  url: ""
+});
+
+redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
+redisClient.on("connect", () => console.log("✅ Redis Connected"));
+
+await redisClient.connect();
+
+export default redisClient;
