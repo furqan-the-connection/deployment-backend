f0cac62b9d9738212a7a022e86b85d445997c67a
Furqan <furqan4fev2004khan@gmail.com>
2025-07-17 16:19:39 +0500

    pushing

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      "https://mernb-2-frontend.vercel.app"
+     "https://deployment-frontend-ten.vercel.app"
     ],
     //  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     // allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
